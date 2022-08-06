8677f7c8b96a3ca61b548eaa5e49592ca596bcd9
siemens345 <semenkucma@gmail.com>
2022-08-06 17:37:12 +0500

    edit formatting

--- src/test/java/githubsearch/GithubSearch.java
+++ src/test/java/githubsearch/GithubSearch.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import java.io.File;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -13,8 +13,8 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class GithubSearch {
     @BeforeAll
-    static void configure(){
-        Configuration.baseUrl="https://github.com/";
+    static void configure() {
+        Configuration.baseUrl = "https://github.com/";
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
     }
@@ -23,13 +23,15 @@ public class GithubSearch {
     void searchSelenide() {
         open("/selenide/selenide");
         $(By.id("wiki-tab")).click();
-        $(By.id("wiki-body")).shouldHave(text("Soft assertions"));
+        $(byCssSelector(".js-wiki-more-pages-link")).click();
+        $(byCssSelector(".Box--condensed")).shouldHave(text("Soft assertions"));
         $(byText("Soft assertions")).click();
         $(By.id("wiki-body")).shouldHave(text("Using JUnit5 extend test class"));
     }
-        @AfterAll
-        static void closing(){
-            closeWebDriver();
+
+    @AfterAll
+    static void closing() {
+        closeWebDriver();
     }
 }
 

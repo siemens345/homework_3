5c1c6eccaaf7208de5152a8575d6df59ad6ff448
siemens345 <semenkucma@gmail.com>
2022-08-06 16:43:25 +0500

    Third homework

---
+++ src/test/java/githubsearch/GithubSearch.java
@@ -0,0 +1,35 @@
+package githubsearch;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import java.io.File;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GithubSearch {
+    @BeforeAll
+    static void configure(){
+        Configuration.baseUrl="https://github.com/";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void searchSelenide() {
+        open("/selenide/selenide");
+        $(By.id("wiki-tab")).click();
+        $(By.id("wiki-body")).shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $(By.id("wiki-body")).shouldHave(text("Using JUnit5 extend test class"));
+    }
+        @AfterAll
+        static void closing(){
+            closeWebDriver();
+    }
+}
+
